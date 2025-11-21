74ef50b81686ca4fc9ce1112e184477110f07832
facephibot <admin@facephi.com>
2025-11-21 13:38:39 +0000

    Update examples folder

---
+++ examples/lit/sdk-lit-file-uploader/src/AppRoot.js
@@ -0,0 +1,178 @@
+/**
+ * Facephi SDK Provider Configuration Example
+ *
+ * WARNING:
+ * This is an example of the implementation of the Web SDK Library.
+ * All the properties, events and methods used in this examples are implemented as orientation to a better performance in coding.
+ *
+ * Please, consider to check the documentation before editing the code.
+ *
+ * We recommend to remove all the console logs and use actual code.
+ *
+ */
+import { LitElement, html } from "lit";
+import { Logger, LoggerType } from "./utils/Logger.js";
+
+export class App extends LitElement {
+  static properties = {
+    widget: { type: String },
+    licenseKey: { type: String },
+  };
+
+  constructor() {
+    super();
+    this.widget = "fileUploader";
+    this.licenseKey = import.meta.env.VITE_LICENSE_KEY || "";
+    this.maxFiles = 2;
+    this.maxFileSize = 5;
+    this.showLog = false;
+    this.language = "es";
+    this.previewImage = true;
+    this.captureTimeout = 10;
+    this.captureRetries = 3;
+    this.stabilizationStage = true;
+    this.interactible = false;
+    this.timeout = 30000;
+  }
+
+  // Renders the content in the main DOM instead of using shadow DOM.
+  createRenderRoot() {
+    return this;
+  }
+
+  firstUpdated() {
+    this.setupSDKProvider();
+  }
+
+  setupSDKProvider() {
+    customElements.whenDefined("facephi-sdk-provider").then(() => {
+      const sdkProvider = this.querySelector("facephi-sdk-provider");
+      if (sdkProvider) {
+        sdkProvider.addEventListener(
+          "emitData",
+          this.handleEmitData.bind(this)
+        );
+        sdkProvider.addEventListener(
+          "emitError",
+          this.handleEmitError.bind(this)
+        );
+      }
+    });
+  }
+
+  handleEmitData(event) {
+    const result = event.detail;
+    Logger.printLog(LoggerType.SDK_PROVIDER, "onEmitData", result);
+  }
+
+  handleEmitError(event) {
+    const result = event.detail;
+    Logger.printLog(LoggerType.SDK_PROVIDER, "onEmitError", result);
+  }
+
+  handleUploadError(event) {
+    const result = event.detail;
+    Logger.printLog(LoggerType.SDK_PROVIDER, "uploadError", result);
+  }
+
+  handleSelphidEvents(e) {
+    const type = e.type;
+    const result = e.detail?.detail;
+    Logger.printLog(LoggerType.SELPHID, type, result);
+    if (type === "extractionFinish") {
+      this.widget = "selphi";
+    }
+  }
+
+  handleSelphiEvents(e) {
+    const type = e.type;
+    const result = e.detail?.detail;
+    Logger.printLog(LoggerType.SELPHI, type, result);
+    if (type === "extractionFinish") {
+      this.widget = "finish";
+    }
+  }
+
+  handleFileUpload(e) {
+    const type = e.type;
+    const result = e.detail?.detail;
+    Logger.printLog(LoggerType.SELPHI, type, result);
+    if (type === "uploadFinish") {
+      this.widget = "selphid";
+      console.log("fileUploaded:", e.detail);
+    }
+  }
+
+  render() {
+    return html`
+      <main class="main">
+        <section class="sdk-section">
+          <facephi-sdk-provider
+            apikey="${this.licenseKey}"
+            steps="START,SELPHID_WIDGET,SELPHI_WIDGET,FINISH"
+            type="ONBOARDING"
+            customer-id="facephi-sdk-lit-example"
+            language="${this.language}"
+          >
+            ${this.widget === "fileUploader"
+              ? html`
+                  <facephi-file-uploader-widget
+                    max-files="${this.maxFiles}"
+                    max-file-size="${this.maxFileSize}"
+                    @uploadFinish=${this.handleFileUpload}
+                    @uploadError=${this.handleUploadError}
+                  ></facephi-file-uploader-widget>
+                `
+              : ""}
+            ${this.widget === "selphid"
+              ? html`
+                  <facephi-selphid-widget
+                    country="${this.country}"
+                    preview-image="${this.previewImage}"
+                    capture-timeout="${this.captureTimeout}"
+                    capture-retries="${this.captureRetries}"
+                    show-log="${this.showLog}"
+                    @moduleLoaded=${this.handleSelphidEvents}
+                    @extractionFinish=${this.handleSelphidEvents}
+                    @extractionTimeout=${this.handleSelphidEvents}
+                    @exceptionCaptured=${this.handleSelphidEvents}
+                    @timeoutErrorButtonClick=${this.handleSelphidEvents}
+                    @errorTimeout=${this.handleSelphidEvents}
+                    @trackStatus=${this.handleSelphidEvents}
+                    @userCancel=${this.handleSelphidEvents}
+                  ></facephi-selphid-widget>
+                `
+              : ""}
+            ${this.widget === "selphi"
+              ? html`
+                  <facephi-selphi-widget
+                    stabilization-stage="${this.stabilizationStage}"
+                    interactible="${this.interactible}"
+                    preview-image="${this.previewImage}"
+                    timeout="${this.timeout}"
+                    show-log="${this.showLog}"
+                    @moduleLoaded=${this.handleSelphiEvents}
+                    @extractionFinish=${this.handleSelphiEvents}
+                    @extractionTimeout=${this.handleSelphiEvents}
+                    @exceptionCaptured=${this.handleSelphiEvents}
+                    @timeoutErrorButtonClick=${this.handleSelphiEvents}
+                    @errorTimeout=${this.handleSelphiEvents}
+                    @userCancel=${this.handleSelphiEvents}
+                    @trackStatus=${this.handleSelphiEvents}
+                    @stabilizing=${this.handleSelphiEvents}
+                  ></facephi-selphi-widget>
+                `
+              : ""}
+            ${this.widget === "finish"
+              ? html`
+                  <div class="onboarding-finished">ONBOARDING FINISHED</div>
+                `
+              : ""}
+          </facephi-sdk-provider>
+        </section>
+      </main>
+    `;
+  }
+}
+
+customElements.define("app-component", App);
