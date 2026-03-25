bfa51373f9e83aa2d9e220579c98804c1eb5241a
Enrique Aparicio <enriqueaparicio@facephi.com>
2026-03-25 13:11:24 +0100

    Add initial Angular 20 SDK file uploader example with configuration files, components, and styles

---
+++ examples/angular/v20/sdk-file-uploader/src/app/app.component.ts
@@ -0,0 +1,24 @@
+/**
+ * Facephi SDK Provider Configuration Example
+ *
+ * WARNING:
+ * This is an example of the implementation of the Web SDK Library.
+ * All the properties, events and methods used in this examples are implemented as orientation to a better performance in coding.
+ *
+ * Please, consider to check the documentation before editing the code.
+ *
+ * We recommend to remove all the console logs and use actual code.
+ *
+ */
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+
+@Component({
+	selector: 'app-root',
+	standalone: true,
+	imports: [RouterOutlet],
+	schemas: [],
+	templateUrl: './app.component.html',
+})
+export class AppComponent {}
---
+++ examples/angular/v20/sdk-file-uploader/src/components/finish-component/finish.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'finish-component',
+	standalone: true,
+	imports: [],
+	templateUrl: './finish.component.html',
+	styleUrl: './finish.component.css',
+})
+export class FinishComponent {}
